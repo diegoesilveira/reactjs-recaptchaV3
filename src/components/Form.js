d4cee9173bb0e513e4922eb498b3bb2f66cd9747
emerson <emersonl.lourenzi@gmail.com>
2020-09-25 13:18:20 -0300

    Atualização.

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,11 +5,14 @@ import RaisedButton from "material-ui/RaisedButton";
 import Recaptcha from "react-google-invisible-recaptcha";
 import RecaptchaV2 from "react-google-recaptcha";
 import api from "../services/api";
+import api1 from "../services/api";
 
  export class Form extends Component {
    state = {
      visible: true,
      isVerified: false,
+       logado: false,
+       loading: false
    };
 
    sendMessage = ({ visible }) => {
@@ -20,7 +23,6 @@ import api from "../services/api";
          alert("Token V3:  " + value);
        });
      } else {
-
        alert("Token V2: " + this.recaptcha.getValue());
        this.sendTokenV2(this.recaptcha.getValue());
      }
@@ -32,8 +34,12 @@ import api from "../services/api";
          token: value,
        })
        .then(({ data }) => {
-         alert("Pontuação: " + data);
-          this.setState({ visible: false });
+         alert("Pontuação: " + data.score);
+            if(data.score < 1){
+                this.setState({ visible: false, logado: false  });
+            }else{
+                this.setState({ visible: true, logado: true });
+            }
        })
        .catch((error) => {
          alert("Falha de comunicação");
@@ -46,9 +52,16 @@ import api from "../services/api";
        .post("/recaptchav2", {
          token: value,
        })
-       .then(({ data }) => {
-         alert("Pontuação: " + data);
- this.setState({ visible: true });
+       .then(async ({ data }) =>{
+           console.log('dados v2',data);
+         alert("Pontuação: " + data.score);
+            await this.setState({ loading:true });
+           if(data.success){
+               await this.setState({ visible: true, logado: true  });
+           }else{
+               await this.setState({ visible: false, logado: false });
+               await this.setState({ loading:false });
+           }
        })
        .catch((error) => {
          alert("Falha de comunicação");
@@ -59,10 +72,11 @@ import api from "../services/api";
      return (
        <MuiThemeProvider>
          <React.Fragment>
-           <AppBar title="Recaptcha Demo" />
+           <AppBar title={this.state.logado ? "Logado" : "Recaptcha Demo"} />
        
            <br />
            <br />
+
            <RaisedButton
              label="Enviar token"
              style={StyleSheet.button}
@@ -73,11 +87,11 @@ import api from "../services/api";
            {this.state.visible ? (
              <Recaptcha
                ref={(ref) => (this.recaptcha = ref)}
-               sitekey="6Lc9ZLIZAAAAAJUKhKsO1B0K62l41V45bGbkYMg5"
+               sitekey="6Lfi_LAZAAAAAHtCuTsaB9kEbvRqvcREmNtdFiCf"
 
-            />) : (<div><RecaptchaV2 ref={(ref) => (this.recaptcha = ref)}
-                 sitekey="6LeYuskZAAAAAI1UI_n3_eBK4BCWSEw6XIFARVnT"
-               /> </div>)}
+            />) : !this.state.loading ? (<div><RecaptchaV2 ref={(ref) => (this.recaptcha = ref)}
+                 sitekey="6LcztrwZAAAAADEmAqbYHp8Judq8f7hL3yocoWi7"
+               /> </div>) : <h1>Carregando...</h1>}
          </React.Fragment>
        </MuiThemeProvider>
      );
